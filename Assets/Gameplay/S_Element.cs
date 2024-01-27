ad10183d5a690aeb68f93638c1ac682832308ecc
anouri <anourigat@artfx.fr>
2024-01-27 12:44:10 +0100

    Wip

--- Assets/Gameplay/S_Element.cs
+++ Assets/Gameplay/S_Element.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class S_Element : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LayerMask layer;
+
+    private void OnCollisionEnter(Collision other)
     {
-        
+        print("Collide1");
+
+        if (other.gameObject.layer == 7)
+        {
+            print("Collide");
+            foreach (ContactPoint contact in other.contacts)
+            {
+                transform.parent = other.transform;
+                break;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
